a74ae424d7775b6e3d54e43df6aeb8b1a253abfc
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-08-21 12:45:29 +1200

    working on contact page

--- codewebsite.py
+++ codewebsite.py
@@ -6,6 +6,18 @@ def home():
 @app.route('/upcoming')
 def upcoming():
     return render_template('upcoming.html')
+@app.route('/previous')
+def previous():
+    return render_template('previous.html')
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+@app.route('/interact')
+def interact():
+    return render_template('interact.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
